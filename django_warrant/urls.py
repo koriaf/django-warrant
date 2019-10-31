89ce24edfa0fe0812b58254beba57602cafb0eb8
Arkadiy Korotaev <koriaf@gmail.com>
2019-10-31 11:14:16 +0100

    feat<django2.2>: update urls.py to avoid removed function usage

--- django_warrant/urls.py
+++ django_warrant/urls.py
@@ -1,18 +1,43 @@
-
 from django.contrib.auth import views as auth_views
 from django.urls import re_path
 
-from .views import ProfileView,UpdateProfileView,MySubsriptions,\
-    AdminListUsers,AdminSubscriptions,LogoutView
+from .views import (
+    ProfileView,
+    UpdateProfileView,
+    MySubsriptions,
+    AdminListUsers,
+    AdminSubscriptions,
+    LogoutView,
+)
 
-app_name = 'dw'
+app_name = "dw"
+
+if hasattr(auth_views, "login"):
+    login_view = auth_views.login
+else:
+    login_view = auth_views.LoginView.as_view(template_name="warrant/login.html")
 
 urlpatterns = (
-    re_path(r'^login/$', auth_views.login, {'template_name': 'warrant/login.html'}, name='login'),
-    re_path(r'^logout/$', LogoutView.as_view(), {'template_name': 'warrant/logout.html'}, name='logout'),
-    re_path(r'^profile/$', ProfileView.as_view(),name='profile'),
-    re_path(r'^profile/update/$', UpdateProfileView.as_view(),name='update-profile'),
-    re_path(r'^profile/subscriptions/$', MySubsriptions.as_view(),name='subscriptions'),
-    re_path(r'^admin/cognito-users/$', AdminListUsers.as_view(),name='admin-cognito-users'),
-    re_path(r'^admin/cognito-users/(?P<username>[-\w]+)$', AdminSubscriptions.as_view(),name='admin-cognito-user')
+    re_path(
+        r"^login/$", login_view, {"template_name": "warrant/login.html"}, name="login"
+    ),
+    re_path(
+        r"^logout/$",
+        LogoutView.as_view(),
+        {"template_name": "warrant/logout.html"},
+        name="logout",
+    ),
+    re_path(r"^profile/$", ProfileView.as_view(), name="profile"),
+    re_path(r"^profile/update/$", UpdateProfileView.as_view(), name="update-profile"),
+    re_path(
+        r"^profile/subscriptions/$", MySubsriptions.as_view(), name="subscriptions"
+    ),
+    re_path(
+        r"^admin/cognito-users/$", AdminListUsers.as_view(), name="admin-cognito-users"
+    ),
+    re_path(
+        r"^admin/cognito-users/(?P<username>[-\w]+)$",
+        AdminSubscriptions.as_view(),
+        name="admin-cognito-user",
+    ),
 )
